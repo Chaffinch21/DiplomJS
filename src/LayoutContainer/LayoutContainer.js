c248db59c40af5fee1ccebcbb3d6453a268139a2
Гардт Анна <anna261187@mail.ru>
2021-07-07 00:22:55 +0700

    added react-router

--- src/LayoutContainer/LayoutContainer.js
+++ src/LayoutContainer/LayoutContainer.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 
 const LayoutContainer = () => {
   return(
-    <span>
-      LayoutContainer
-    </span>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={'/'}>
+          <span>Main</span>
+        </Route>
+        <Route path={'/photo-item'}>
+          <span>PhotoItem</span>
+        </Route>
+        <Route path={'/photos'}>
+          <span>Photos</span>
+        </Route>
+        <Route path={'/auth'}>
+          <span>Authorization</span>
+        </Route>
+        <Route path={'*'}>
+          <span>Error</span>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 };
 
